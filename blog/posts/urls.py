ef700cbed108f18c5ea2d688f094982e84a80044
Umesh <umeshtamang129@gmail.com>
2025-04-15 17:28:48 +0545

    first views and urls

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    
+    path('',views.post_list),
+    path('create/',views.post_create),
+    path('detail/',views.post_detail),
+    path('update/',views.post_update),
+    path('delete/',views.post_delete),
 ]
