251e6fe34baf18ec1fefbc80592ef3f385522ba4
Umesh <umeshtamang129@gmail.com>
2025-04-16 08:50:59 +0545

    url links

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns = [
     path('',views.post_list),
     path('create/',views.post_create),
-    path('detail/<int:id>/',views.post_detail),
+    path('detail/<int:id>/',views.post_detail,name="detail"),
     path('update/',views.post_update),
     path('delete/',views.post_delete),
 ]
