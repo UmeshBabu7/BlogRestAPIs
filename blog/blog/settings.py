4d3cdacc27f719746bb63092f738f6c5e93be8f7
Umesh <umeshtamang129@gmail.com>
2025-04-19 16:41:07 +0545

    setup static,media

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -106,7 +106,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static"),
+
+ ]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
